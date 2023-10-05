1b66a6c75a612257184895fc34649ea36211a51f
TonySD <ahtoxa.b@gmail.com>
2023-10-05 18:47:24 +0300

    Impl: if config not exists, buddy config will be created

--- src/main/java/com/example/curatorBot/configParser.java
+++ src/main/java/com/example/curatorBot/configParser.java
@@ -1,7 +1,10 @@
 package com.example.curatorBot;
 
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -10,20 +13,32 @@ public class configParser {
     public static String propertiesFilePath = "src/main/resources/config.properties";
 
     static {
+        properties = new Properties();
         load();
     }
 
     public static void load() {
         FileInputStream fis;
-        properties = new Properties();
-
         try {
             fis = new FileInputStream(propertiesFilePath);
             properties.load(fis);
         } catch (FileNotFoundException e) {
             System.err.printf("[ERROR] File %s not exists \n", propertiesFilePath);
+            System.err.println("[INFO] Creating BUDDY properties");
+            createBuddyProperties();
         } catch (IOException e) {
             System.err.println("[ERROR] Failed loading properties");
+        }
+    }
+
+    private static void createBuddyProperties() {
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = new FileOutputStream(propertiesFilePath);
+            // add properties here
+            properties.store(fileOutputStream, null);
+        } catch (SecurityException | IOException exception) {
+            System.err.println(exception.getMessage());
         }
     }
 }
