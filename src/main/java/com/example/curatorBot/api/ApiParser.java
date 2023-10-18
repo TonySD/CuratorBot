9b3107c0685746a593cb9e89927fdf4f5ea28ab2
TonySD <ahtoxa.b@gmail.com>
2023-10-19 00:22:38 +0300

    UPD: Cookies now takes from CookieValidator

--- src/main/java/com/example/curatorBot/api/ApiParser.java
+++ src/main/java/com/example/curatorBot/api/ApiParser.java
@@ -1,12 +1,9 @@
 package com.example.curatorBot.api;
 
-import com.example.curatorBot.api.dto.HwPages;
-import com.example.curatorBot.api.dto.PassedHW;
+import com.example.curatorBot.api.dto.HomeworkPages;
+import com.example.curatorBot.api.dto.ParsedHomework;
 import com.example.curatorBot.configParser;
-import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.log4j.PropertyConfigurator;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,53 +15,24 @@ import java.util.Optional;
 
 @Log4j
 public class ApiParser {
-    private static int homeworks = 0;
-    private static Map<String, String> cookies;
+    private static int homeworks;
+
 
     static {
-        updateCookie();
+        getHWInfo();
     }
 
     public static void main(String[] args) {
         System.out.println(getHWInfo().orElseThrow());
     }
 
-    public static void checkCookieForUpdate() {
-        try {
-            Connection.Response res = Jsoup
-                    .connect(configParser.getProperty("api.index_url"))
-                    .cookies(cookies)
-                    .followRedirects(true)
-                    .execute();
-            if (res.url().toString().equals(configParser.getProperty("api.login_url"))) {
-                log.info("Cookies was invalid - updating");
-                updateCookie(); // If redirects to login page - cookie invalidated
-            } else log.trace("Cookies are valid");
-        } catch (IOException ignored) {}
-    }
 
 
-    private static void updateCookie() {
-        Connection.Response res;
-        try {
-            res = Jsoup
-                    .connect(configParser.getProperty("api.login_url"))
-                    .data("email", configParser.getProperty("api.email"), "password", configParser.getProperty("api.password"))
-                    .method(Connection.Method.POST)
-                    .execute();
-            cookies = res.cookies();
-            log.info("Cookies was updated");
-        } catch (IOException exception) {
-            log.warn(exception.getMessage());
-        }
-    }
-
-    public static Optional<PassedHW> getSelectedHW(String url) {
-        checkCookieForUpdate();
+    public static Optional<ParsedHomework> getSelectedHW(String url) {
         Document doc;
         try {
             doc = Jsoup.connect(url)
-                    .cookies(cookies)
+                    .cookies(CookieValidator.getCookies())
                     .get();
             String student_name = doc
                     .select("#mainForm > div:nth-child(1) > div > div > div:nth-child(2) > div > div.row > div:nth-child(1) > input")
@@ -82,7 +50,7 @@ public class ApiParser {
             int max_mark = Integer.parseInt(mark[1]);
             log.debug("Homework was parsed successfully");
 
-            return Optional.of(new PassedHW(
+            return Optional.of(new ParsedHomework(
                     student_name,
                     lesson_name,
                     student_mark,
@@ -95,18 +63,18 @@ public class ApiParser {
         return Optional.empty();
     }
 
-    private static Optional<HwPages> getHWInfo() {
-        checkCookieForUpdate();
+    private static Optional<HomeworkPages> getHWInfo() {
         Document doc;
         try {
             doc = Jsoup.connect(configParser.getProperty("api.homeworks_url"))
-                    .cookies(cookies)
+                    .cookies(CookieValidator.getCookies())
                     .get();
             Element data = doc.select("#example2_info").first();
             String[] result = data.select("div").text().split(" ");
             result[4] = result[4].substring(0, result[4].length() - 1);
-            return Optional.of(new HwPages(
-                    Integer.parseInt(result[6]),
+            homeworks = Integer.parseInt(result[6]);
+            return Optional.of(new HomeworkPages(
+                    homeworks,
                     Integer.parseInt(result[4])
             ));
         } catch (IOException exception) {
@@ -114,5 +82,11 @@ public class ApiParser {
         }
         return Optional.empty();
 
+    }
+
+    private static boolean checkNewHWs() {
+        Optional<HomeworkPages> result = getHWInfo();
+        if (result.isEmpty()) return false;
+        return result.get().amount_of_HWS() == homeworks;
     }
 }
