d36b4d7343445b69c2c134af1711b0cf7891de32
TonySD <ahtoxa.b@gmail.com>
2023-10-20 14:10:38 +0300

    Fix: HomeworkDTO now can extract id from url

--- src/main/java/com/example/curatorBot/api/dto/HomeworkDTO.java
+++ src/main/java/com/example/curatorBot/api/dto/HomeworkDTO.java
@@ -4,13 +4,19 @@ import com.example.curatorBot.api.ApiParser.ApiParser;
 import com.example.curatorBot.configParser;
 
 public class HomeworkDTO {
-    private ParsedHomework hw_info = null;
     private final long id;
     private final String url;
 
     public HomeworkDTO(long id) {
         this.id = id;
         url = String.format(configParser.getProperty("api.selected_homework_url"), id);
+    }
+
+    public HomeworkDTO(String url) {
+        this.url = url;
+        int end_index = url.indexOf('?');
+        int first_index = url.lastIndexOf('/');
+        id = Integer.parseInt(url.substring(first_index + 1, end_index));
     }
 
     public long getId() {
