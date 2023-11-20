a955bbb28747b0e380656e2d77675344b6d1f895
TonySD <ahtoxa.b@gmail.com>
2023-11-20 17:05:06 +0300

    Feat: some naming fixes

--- src/main/java/com/example/curatorBot/API/validators/ApiAuthValidator.java
+++ src/main/java/com/example/curatorBot/API/validators/ApiAuthValidator.java
@@ -1,4 +1,4 @@
-package com.example.curatorBot.API.cookies;
+package com.example.curatorBot.API.validators;
 
 import com.example.curatorBot.configParser;
 import lombok.extern.log4j.Log4j2;
@@ -8,27 +8,27 @@ import org.jsoup.Jsoup;
 import java.io.IOException;
 
 @Log4j2
-public class ApiCookieValidator extends CookieValidator {
-    public ApiCookieValidator() {
-        updateCookie();
+public class ApiAuthValidator extends Validator {
+    public ApiAuthValidator() {
+        updateAuth();
     }
 
-    public void checkCookieForUpdate() {
+    public void checkValidity() {
         try {
             Connection.Response res = Jsoup
                     .connect(configParser.getProperty("api.index_url"))
-                    .cookies(cookies)
+                    .cookies(auth_data)
                     .followRedirects(true)
                     .execute();
             if (res.url().toString().equals(configParser.getProperty("api.login_url"))) {
                 log.info("Cookies was invalid - updating");
-                updateCookie(); // If redirects to login page - cookie invalidated
+                updateAuth(); // If redirects to login page - cookie invalidated
             } else log.trace("Cookies are valid");
         } catch (IOException ignored) {}
     }
 
 
-    protected void updateCookie() {
+    protected void updateAuth() {
         Connection.Response res;
         try {
             res = Jsoup
@@ -36,7 +36,7 @@ public class ApiCookieValidator extends CookieValidator {
                     .data("email", configParser.getProperty("api.email"), "password", configParser.getProperty("api.password"))
                     .method(Connection.Method.POST)
                     .execute();
-            cookies = res.cookies();
+            auth_data = res.cookies();
             log.info("Cookies was updated");
         } catch (IOException exception) {
             log.warn(exception.getMessage());
